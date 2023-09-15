e12b3b1283a58d07ee934c8c58c6658e43313c17
Ben Kinsey <bkinsey@gmail.com>
2023-09-15 13:33:45 -1000

    some work on external links lesson

--- src/utils.js
+++ src/utils.js
@@ -74,10 +74,32 @@ export const lessons = [
     key: "internal-links",
     title: "Internal Links",
   },
+  {
+    key: "anatomy-url",
+    title: "Anatomy of a URL",
+  },
+  {
+    key: "external-links",
+    title: "External Links",
+  },
 ];
 
+/**
+ * gets the lesson number from the path
+ * @return {number}
+ */
 export const getLessonNumber = () => {
   const { pathname } = new URL(window.location.href);
   const digits = pathname.match(/\d+/)?.[0];
   return parseInt(digits ?? "", 10);
 };
+
+/**
+ * gets the path to a lesson from the lesson number
+ * @param {number} lessonNumber
+ * @return {string}
+ */
+export const getLessonPath = (lessonNumber) =>
+  `/lesson/${String(lessonNumber).padStart(3, "0")}-${
+    lessons[lessonNumber - 1].key
+  }.html`;
---
+++ src/web-components/lesson-button/lesson-button.js
@@ -0,0 +1,61 @@
+import {
+  shadowAppendTemplate,
+  getLessonNumber,
+  lessons,
+  getLessonPath,
+} from "../../utils.js";
+
+const TAG = "lesson-button";
+
+/**
+ * @class LessonButton
+ * @classdesc lesson button
+ * @extends HTMLElement
+ */
+class LessonButton extends HTMLElement {
+  /**
+   * @constructor
+   */
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    const type = this.getAttribute("type") ?? "next";
+
+    shadowAppendTemplate(shadowRoot, TAG).then(() => {
+      // coerce to never be null
+      this.button = /** @type {HTMLButtonElement} */ (
+        shadowRoot.querySelector("button")
+      );
+
+      const lessonNumber = getLessonNumber();
+      const newLessonNumber =
+        type === "next" ? lessonNumber + 1 : lessonNumber - 1;
+
+      if (
+        type === "next" ? lessonNumber > lessons.length - 1 : lessonNumber < 2
+      ) {
+        this.button.innerHTML = "Back to Home";
+        this.button?.addEventListener("click", () => {
+          window.location.href = "/";
+        });
+        return;
+      }
+
+      if (!lessonNumber || newLessonNumber < 1) {
+        this.button.style.display = "none";
+        return;
+      }
+
+      const { title } = lessons[newLessonNumber - 1];
+      this.button.innerHTML = `${
+        type === "next" ? "Continue" : "Back"
+      } to Lesson ${newLessonNumber}: ${title}`;
+
+      this.button?.addEventListener("click", () => {
+        window.location.href = getLessonPath(newLessonNumber);
+      });
+    });
+  }
+}
+
+customElements.define(TAG, LessonButton);
