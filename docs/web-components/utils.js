e1d959d677c693ec246f376ef0f2019207762cda
Ben Kinsey <bkinsey@gmail.com>
2023-09-16 14:47:34 -1000

    deploy

--- docs/web-components/utils.js
+++ docs/web-components/utils.js
@@ -21,7 +21,7 @@ const webComponentTemplatePromises = {};
  */
 const getWebComponentTemplate = async (templateName) => {
   const origin = new URL(window.location.href).origin;
-  const templatePath = `${origin}/web-components/${templateName}/${templateName}.html`;
+  const templatePath = `../web-components/${templateName}/${templateName}.html`;
   await insertHtml(templatePath);
   const el = Promise.resolve(
     /** @type {HTMLTemplateElement | null} */ (
